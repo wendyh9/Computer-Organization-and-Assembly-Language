4e3ee5b2ca9db665fbcd0e1ce036be3137e2a7aa
Wendy <wendyhaw9@gmail.com>
2023-03-31 16:19:08 -0700

    create fib.s

---
+++ More_Procedures_in_MIPS/fib.s
@@ -0,0 +1,30 @@
+        .data
+#n:      .word 13
+
+prompt: .asciiz "Please enter a number: "
+
+        .text
+main: 	la 	 $a0, prompt
+	li 	 $v0, 4
+	syscall
+	li	 $v0, 5
+	syscall
+	add 	 $t3, $v0, $t3
+	add     $t0, $0, $zero
+	addi    $t1, $zero, 1
+	#la      $t3, n
+	#lw      $t3, 0($t3)
+		
+fib: 	beq     $t3, $0, finish
+	add     $t2,$t1,$t0
+	move    $t0, $t1
+	move    $t1, $t2
+	subi    $t3, $t3, 1
+	j       fib
+		
+finish: addi    $a0, $t0, 0
+	li      $v0, 1		
+	syscall			
+	li      $v0, 10		
+	syscall			
+
