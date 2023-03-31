82229e856e750e515f5b792165ed541c32f75684
Wendy <wendyhaw9@gmail.com>
2023-03-31 12:10:17 -0700

    create appendTest.c

---
+++ GDB_and_Pointers/appendTest.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+/*
+    Return the result of appending the characters in s2 to s1.
+    Assumption: enough space has been allocated for s1 to store the extra
+    characters.
+*/
+char* append(char s1[], char s2[]) {
+    int s1len = strlen(s1);
+
+    int s2len = strlen(s2);
+
+    int k;
+    for (k = 0; k < (10 - (s2len + 1)); k++) {
+        s1[k + s1len] = s2[k];
+        if (s2[k + 1] == '\0'){
+            s1[s1len + s2len] = '\0';
+            break;
+        }
+        else if (k == (10 - (s2len + 2))){
+            s1[9] = '\0';
+            break;
+        }
+    }
+    return s1;
+}
+int main() {
+    char str1[10];
+    char str2[10];
+    while (1) {
+        printf("str1 = ");
+        scanf("%[^\n]%*c", str1); 
+        //if (!gets(str1)) {
+        //    return 0;
+        //}
+        printf("str2 = ");
+        scanf("%[^\n]%*c", str2);  
+        //if (!gets(str2)) {
+        //    return 0;
+        //}
+        printf ("The result of appending str2 to str1 is %s.\n", append(str1, str2));
+        fflush(stdin);
+    }
+    return 0;
+}
